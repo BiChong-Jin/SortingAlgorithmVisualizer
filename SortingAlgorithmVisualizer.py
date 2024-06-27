003c427776c19a162a5c779f9482784aa11a420d
Jin <jinjapan97@gmail.com>
2024-06-27 13:54:05 +0900

    changed some code

--- SortingAlgorithmVisualizer.py
+++ SortingAlgorithmVisualizer.py
@@ -53,7 +53,7 @@ def draw(draw_info, algo_name, ascending):
 	title = draw_info.LARGE_FONT.render(f"{algo_name} - {'Ascending' if ascending else 'Descending'}", 1, draw_info.GREEN)
 	draw_info.window.blit(title, (draw_info.width/2 - title.get_width()/2 , 5))
 
-	controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, draw_info.WHITE)
+	controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | D - Descending | A - Ascending", 1, draw_info.WHITE)
 	draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2 , 45))
 
 	sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.WHITE)
